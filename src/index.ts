05ba8dc8ffc700c4acabb757b14a5e5593863145
louai <lbo@big-mama.io>
2024-08-12 14:05:39 +0100

    chore: add rate limiting middlware and diffrent security middlware

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,30 @@ import morgan from "morgan";
 import express, { type Request, type Response } from "express";
 const app = express();
 
+import rateLimit from "express-rate-limit";
 import connectDB from "@/utils/db-connect";
+import xssClean from "xss-clean";
+import helmet from "helmet";
+import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize";
 
+// Logging
 app.use(process.env.ENVIRONMENT === "DEV" ? morgan("dev") : morgan("common"));
+
+// Rate Limiting
+app.set("trust proxy", 1);
+app.use(
+	rateLimit({
+		windowMs: 15 * 60 * 1000,
+		max: 60,
+	}),
+);
+
+// Security
+app.use(helmet());
+app.use(cors());
+app.use(xssClean());
+app.use(mongoSanitize());
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello there");
