ec16f189842c58dc38b179c4af2b516d811c452a
louai <lbo@big-mama.io>
2024-08-13 15:36:01 +0100

    refactor: refactor error-handling middlware to be more general

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,15 +1,20 @@
-import type { CustomAPIError } from '@/errors'
+import { CustomAPIError } from '@/errors'
 import type { TResponse } from '@/types/response'
 import type { NextFunction, Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
-export const errorHandlerMiddleware = (err: CustomAPIError, _req: Request, res: Response, _next: NextFunction) => {
+export const errorHandlerMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.log(err)
+
   const response: TResponse = {
     success: false,
     error: {
-      message: err.message
+      message: err instanceof CustomAPIError ? err.message : 'Unexpected Error happened'
     },
     data: null
   }
 
-  return res.status(err.statusCode).json(response)
+  const statusCode = err instanceof CustomAPIError ? err.statusCode : StatusCodes.INTERNAL_SERVER_ERROR
+
+  return res.status(statusCode).json(response)
 }
